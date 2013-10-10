890f92d5343fb37b2969f00cc27baaeac076ea04
Yoshiyuki Sakamoto <Yoshi@h89.26.238.10.30790.vlan.kuins.net>
2013-10-11 00:22:01 +0900

    最初のコミットです

---
+++ Gion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gion
+//
+//  Created by Yoshiyuki Sakamoto on 2013/10/11.
+//  Copyright (c) 2013年 HeyaYshirtY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
