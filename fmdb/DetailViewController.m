d2305ef0e986b1beccab4398ce6fefada70b2d7d
Yoshiyuki Sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2013-11-19 16:08:16 +0900

    Detail ざっと完成

--- fmdb/DetailViewController.m
+++ fmdb/DetailViewController.m
@@ -36,13 +36,15 @@
     [super viewDidLoad];
     
     // 単語を表示する部分
-    UIScrollView *tango_view = [[UIScrollView alloc] initWithFrame:CGRectMake(10, 80, 300, 300)];
-    tango_view.contentSize = CGSizeMake(300, 300);
+    UIScrollView *tango_view = [[UIScrollView alloc] initWithFrame:CGRectMake(10, 70, 300, 280)];
+    // tango_view.contentSize = CGSizeMake(300, 300);
     tango_view.backgroundColor = lightColor;
     [[tango_view layer] setCornerRadius:10.0]; // 角を丸く
     [[tango_view layer] setBorderWidth:2.0]; // 境界
     [[tango_view layer] setBorderColor:[darkColor CGColor]];
     [[self view] addSubview:tango_view];
+    
+    int position_y = -65;
     
     // 単語
     if(!word){
@@ -50,22 +52,87 @@
         word = [[UILabel alloc] init];
     }
     word.backgroundColor = darkColor;
-    word.frame = CGRectMake(0, 0, 300, 40);
-    word.font = [UIFont systemFontOfSize:24];
+    word.frame = CGRectMake(0, position_y, 300, 60);
+    word.font = [UIFont boldSystemFontOfSize:24];
     word.textColor = lightTextColor;
-    word.text = text.word;
+    word.text = [NSString stringWithFormat:@"　%@", text.word];
     [tango_view addSubview:word];
+    position_y += 60;
     
-    _text.text = text.text;
-    meaning.text = text.meaning;
-    right.text = [[NSString alloc] initWithFormat:@"%d回",text.right];
-    wrong.text = [[NSString alloc] initWithFormat:@"%d回",text.wrong];
+    position_y += 20;
     
     // 意味
-    UILabel *meaning_index_label = [[UILabel alloc] initWithFrame:CGRectMake(0, 150, 320, 20)];
+    UILabel *meaning_border = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 260, 3)];
+    meaning_border.backgroundColor = darkColor;
+    [tango_view addSubview:meaning_border];
+    UILabel *meaning_index_label = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 60, 30)];
     meaning_index_label.textAlignment = NSTextAlignmentCenter;
+    meaning_index_label.backgroundColor = darkColor;
+    meaning_index_label.textColor = lightTextColor;
     meaning_index_label.text = @"意味";
-    [[self view] addSubview:meaning_index_label];
+    [tango_view addSubview:meaning_index_label];
+    position_y += 35;
+    
+    meaning = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 260, 40)];
+    meaning.numberOfLines = 2;
+    meaning.font = [UIFont systemFontOfSize:16];
+    meaning.text = text.meaning;
+    [tango_view addSubview:meaning];
+    
+    position_y += 60;
+    
+    // 例文
+    UILabel *text_border = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 260, 3)];
+    text_border.backgroundColor = darkColor;
+    [tango_view addSubview:text_border];
+    UILabel *text_index_label = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 60, 30)];
+    text_index_label.textAlignment = NSTextAlignmentCenter;
+    text_index_label.backgroundColor = darkColor;
+    text_index_label.textColor = lightTextColor;
+    text_index_label.text = @"例文";
+    [tango_view addSubview:text_index_label];
+    position_y += 35;
+    
+    _text = [[UILabel alloc] initWithFrame:CGRectMake(20, position_y, 260, 60)];
+    _text.numberOfLines = 3;
+    _text.font = [UIFont systemFontOfSize:16];
+    _text.text = text.text;
+    [tango_view addSubview:_text];
+    
+    // 正解した！
+    UILabel *right_bg = [[UILabel alloc] initWithFrame:CGRectMake(10, 360, 140, 60)];
+    right_bg.backgroundColor = buttonColor;
+    [[right_bg layer] setCornerRadius:10.0];
+    [[self view] addSubview:right_bg];
+    
+    UILabel *right_label = [[UILabel alloc] initWithFrame:CGRectMake(10, 365, 140, 20)];
+    right_label.textAlignment = NSTextAlignmentCenter;
+    right_label.text = @"正解した！";
+    [[self view] addSubview:right_label];
+    
+    right = [[UILabel alloc] initWithFrame:CGRectMake(10, 382, 140, 40)];
+    right.font = [UIFont boldSystemFontOfSize:18];
+    right.textAlignment = NSTextAlignmentCenter;
+    [[self view] addSubview:right];
+    right.text = [[NSString alloc] initWithFormat:@"%d回",text.right];
+    
+    // 間違えた…
+    UILabel *wrong_bg = [[UILabel alloc] initWithFrame:CGRectMake(170, 360, 140, 60)];
+    wrong_bg.backgroundColor = buttonColor;
+    [[wrong_bg layer] setCornerRadius:10.0];
+    [[self view] addSubview:wrong_bg];
+    
+    UILabel *wrong_label = [[UILabel alloc] initWithFrame:CGRectMake(170, 365, 140, 20)];
+    wrong_label.textAlignment = NSTextAlignmentCenter;
+    wrong_label.text = @"間違えた…";
+    [[self view] addSubview:wrong_label];
+    wrong = [[UILabel alloc] initWithFrame:CGRectMake(170, 382, 140, 40)];
+    wrong.font = [UIFont boldSystemFontOfSize:18];
+    wrong.textAlignment = NSTextAlignmentCenter;
+    [[self view] addSubview:wrong];
+    wrong.text = [[NSString alloc] initWithFormat:@"%d回",text.wrong];
+    
+
     
 
     /*
